20b036f7addb70068330b2f2410f5427a8a1dd90
jfmurillo <joseph.murillo1621@gmail.com>
2024-07-12 00:35:55 -0600

    feat(main): added swing dependency

--- POODB/src/main/java/org/poodb/Main.java
+++ POODB/src/main/java/org/poodb/Main.java
@@ -1,5 +1,7 @@
 package org.poodb;
 
+import javax.swing.*;
+import java.awt.*;
 import controller.ConexionController;
 import menu.Menu;
 import view.ConsoleView;
@@ -7,7 +9,31 @@ import view.ConsoleView;
 import java.sql.SQLException;
 
 
-public class Main {
+public class Main extends JFrame{
+
+    public Main(){
+        setTitle("PP_POO");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Cierra la aplicación al cerrar la ventana
+        setLocationRelativeTo(null); // Centra la ventana en la pantalla
+
+        // Agregar componentes a la ventana
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("¡Hola, mundo!");
+        panel.add(label);
+
+        JButton button = new JButton("Haz clic");
+        panel.add(button);
+
+        // Agrega el panel al contenido de la ventana
+        getContentPane().add(panel);
+
+        // Mostrar la ventana
+        setVisible(true);
+    }
+
     public static void main(String[] args) throws SQLException {
         System.out.print("Hello MySQL!");
 
@@ -15,7 +41,12 @@ public class Main {
         ConexionController conexionController = new ConexionController(consoleView);
         conexionController.openConnection();
 
-        Menu menu = new Menu();
-        menu.showMenu();
+//        Menu menu = new Menu();
+//        menu.showMenu();
+        SwingUtilities.invokeLater(() -> {
+            new Main(); // Crear y mostrar la ventana
+        });
     }
 }
+
+
